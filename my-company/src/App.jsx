fcea1cfd6b83410455d5b586557639a37cf23af7
Njiru-ux <ericbishop99@gmail.com>
2026-02-19 19:06:43 +0300

    Interactivity in the contact page

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,26 +4,20 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
-import Contact from './pages/Contact';
+import Contact from './components/Contact';  // Changed from './pages/Contact'
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App" style={{
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column'
-      }}>
+      <div className="App">
         <Navbar />
-        <div style={{ flex: 1 }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
         <Footer />
       </div>
     </BrowserRouter>
