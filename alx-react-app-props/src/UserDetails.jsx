5ceee2330fcd3124adbd8b31241d881b89608c64
Njiru-ux <ericbishop99@gmail.com>
2026-02-19 20:27:57 +0300

    Refactor prop drilling to Context API

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import UserContext from './context/UserContext';  // Updated path
+import UserContext from './context/UserContext';
 
 function UserDetails() {
   const userData = useContext(UserContext);
